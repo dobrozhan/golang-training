797c4f23ec550f8813b5313c2499a33ec0eeb3c4
Oleksandr Dobrozhan <dobrozhan.a@gmail.com>
2022-05-24 14:46:51 +0300

    update package task

--- basic/03_packages/main.go
+++ basic/03_packages/main.go
@@ -3,13 +3,10 @@ package main
 import (
 	"fmt"
 	"math"
-
-	strutil "github.com/odobr/go_crash_course/03_packages/str"
 )
 
 func main() {
 	fmt.Println(math.Floor(2.7))
 	fmt.Println(math.Ceil(2.7))
 	fmt.Println(math.Sqrt(9))
-	fmt.Println(strutil.Reverse("hello"))
 }
